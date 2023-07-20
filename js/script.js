11c6ffb32840d79764005a98991eac4cd7d7876f
RodiaGiordano <rodiagiordano93@gmail.com>
2023-07-20 15:46:33 +0200

    fix: value discount

--- js/script.js
+++ js/script.js
@@ -12,12 +12,15 @@ const userKm = document.getElementById("user_km");
 
 userSubmit.addEventListener("click", function(){
 
+    
 
 let age = parseInt(userAge.value);
 let km = parseInt(userKm.value);  
 
+
 console.log(typeof age)
 console.log(age)
+
 
 
 if (isNaN (age) || isNaN(km)){
@@ -30,16 +33,20 @@ else {
 
     if (age >65){
 
-        ticketPrice *= 0.8;       
+        ticketPrice *= 0.6;       
     }
 
     else if (age <18){
-        ticketPrice *= 0.6;
+        ticketPrice *= 0.8;
     }
 
+    
+    userAge.value = "";
+    userKm.value = "";
 
     document.getElementById("ticket").innerHTML = "il prezzo del biglietto è: " + ticketPrice.toFixed(2) + "€";
     
+   
 }
 
 })
