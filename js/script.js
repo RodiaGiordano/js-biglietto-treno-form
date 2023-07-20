29fec0936983805ef9ecb5e3d846ab3e08b65dd4
RodiaGiordano <rodiagiordano93@gmail.com>
2023-07-20 15:01:01 +0200

    feat: add alpha structure

--- js/script.js
+++ js/script.js
@@ -1,37 +1,66 @@
 
 const price = 0.21;
 
-const userAge = parseInt(prompt ("inserisci la tua età"));
+const userSubmit = document.getElementById("user_submit");
 
-const userKm = parseFloat(prompt ("inserisci il nuemro chilometri che vuoi percorrere"));
+const userAge = document.getElementById("user_age");
 
-if (isNaN (userAge) || isNaN(userKm)){
-    alert("Accetta solo caratteri numerici - premere F5")
-} 
-
-else {
-
-    ticketPrice = price * userKm;
+const userKm = document.getElementById("user_km");
+ 
 
 
 
-if (userAge >65){
-
-    ticketPrice *= 0.8;       
-}
-
-else if (userAge <18){
-    ticketPrice *= 0.6;
-}
+userSubmit.addEventListener("click", function(){
 
 
     
 
 
-document.getElementById("ticket").innerHTML = "il prezzo del biglietto è: " + ticketPrice.toFixed(2) + "€";
+
+    
+console.log(typeof userAge + " Tipo")
+console.log(userAge + " Valore")
+console.log(typeof userKm + "Tipo")
+console.log(userKm + " Valore")   
+
+
+
+
+    if (isNaN (userAge) || isNaN(userKm)){
+        alert("Accetta solo caratteri numerici - premere F5")
+    } 
+
+    else {
+
+        ticketPrice = price * userKm;
+
+
+
+    if (userAge >65){
+
+        ticketPrice *= 0.8;       
+    }
+
+    else if (userAge <18){
+        ticketPrice *= 0.6;
+    }
+
+
+    
+
+
+    document.getElementById("ticket").innerHTML = "il prezzo del biglietto è: " + ticketPrice.toFixed(2) + "€";
 
 
 }
+
+
+
+})
+
+
+
+
 
 
 
